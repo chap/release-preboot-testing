d7b146c4a2e1816f5a6e63d9a7f123867d7b464b
Chap Ambrose <chapambrose@gmail.com>
2019-02-01 12:28:57 -0600

    add dyno id

--- app.rb
+++ app.rb
@@ -2,17 +2,17 @@ require "sinatra"
 
 get '/*' do
   out = []
-  out << "Release Version: #{ENV['HEROKU_RELEASE_VERSION']}"
-  out << "Release Created: #{ENV['HEROKU_RELEASE_CREATED_AT']}"
-
   # created in .profile
   File.open('process_started_at', 'r') do |f|
     while line = f.gets
       out << line
     end
   end
+  out << "Release Version: #{ENV['HEROKU_RELEASE_VERSION']}"
+  out << "Release Created: #{ENV['HEROKU_RELEASE_CREATED_AT']}"
+  out << "Dyno ID: #{ENV['HEROKU_DYNO_ID']}"
 
-  out = out.join("\r")
+  out = out.join("\r\n\r\n")
 
   puts out
   return out
